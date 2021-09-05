e0863f5e6c8de41166424460e5c52f86b702db07
IgnacioZ94 <bs.zayas@gmail.com>
2021-09-05 02:46:53 -0300

    Estandarizacion del front, creacion de componentes y creacion de rutas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
+
+import Navigaton from './components/Navigation';
+import NotesList from './components/NotesList';
+import CreateNote from './components/CreateNote';
+import CreateUser from './components/CreateUser';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Navigaton/>
+        <Route path="/" exact component={NotesList} />
+        <Route path="/edit/:id" component={CreateNote} />
+        <Route path="/create" component={CreateNote} />
+        <Route path="/user" component={CreateUser} />
+      </Router>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
       </header>
     </div>
   );
